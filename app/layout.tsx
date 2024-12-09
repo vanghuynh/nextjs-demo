3f40ef0c421e65595551e95679ec699017499189
Vang Huynh <huynh.vang@devexperts.com>
2024-12-09 10:10:57 +0700

    Nav component and Nextjs Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './globals.css';
+import Navbar from '../components/Navbar';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body>
-        <nav>Hello There</nav>
-        {children}
+      <body className={inter.className}>
+        <Navbar />
+        <main className='max-w-3xl mx-auto py-10'>{children}</main>
       </body>
     </html>
   );
