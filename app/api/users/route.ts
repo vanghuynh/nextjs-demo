d57b2bdc4f3988573dd395e69f288224294223e6
Vang Huynh <huynh.vang@devexperts.com>
2024-12-10 16:08:15 +0700

    Next Request and Next Response

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,12 @@
 import { fetchUsers, saveUser } from '../../../utils/action';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async (req: Request) => {
-  const { searchParams } = new URL(req.url);
-  console.log('searchParams: ', searchParams);
-  const id = searchParams.get('id');
-  console.log('id: ', id);
+export const GET = async (req: NextRequest) => {
+  console.log(req.url);
+  console.log(req.nextUrl.searchParams.get('id'));
+
   const users = await fetchUsers();
 
-  return Response.json({ users: users });
+  //return Response.json({ users: users });
+  return NextResponse.redirect(new URL('/', req.url));
 };
