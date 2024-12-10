79132438d393caaa49d404ae5ea0951334c59dce
Vang Huynh <huynh.vang@devexperts.com>
2024-12-10 16:02:38 +0700

    handle request with params

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,11 @@
 import { fetchUsers, saveUser } from '../../../utils/action';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url);
+  console.log('searchParams: ', searchParams);
+  const id = searchParams.get('id');
+  console.log('id: ', id);
   const users = await fetchUsers();
 
   return Response.json({ users: users });
