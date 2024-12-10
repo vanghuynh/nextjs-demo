a743d93e98a4990fed1d4a9141a2c4a41cb09891
Vang Huynh <huynh.vang@devexperts.com>
2024-12-10 08:53:14 +0700

    Optional catch-all route segment (used by Clerk)

--- app/(dashboard)/auth/[[...sign-in]]/page.tsx
+++ app/(dashboard)/auth/[[...sign-in]]/page.tsx
@@ -1,8 +1,9 @@
 const SignInPage = ({ params }: { params: { 'sign-in': string[] } }) => {
   console.log(params);
+  console.log(params['sign-in'][0]);
   return (
     <div>
-      <h1 className='text-7xl'>Sign In Page</h1>
+      <h1 className='text-7xl'>Sign In Page: {params['sign-in'][0]}</h1>
     </div>
   );
 };
--- app/(dashboard)/auth/page.tsx
+++
@@ -1,5 +0,0 @@
-const AuthPage = () => {
-  return <h1>Auth Page</h1>;
-};
-
-export default AuthPage;
