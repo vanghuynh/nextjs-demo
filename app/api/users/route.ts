80fba15933d520f4ce4bab1a7d563e96c38fa1ed
Vang Huynh <huynh.vang@devexperts.com>
2024-12-11 08:43:48 +0700

    post rest api create user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,6 +7,14 @@ export const GET = async (req: NextRequest) => {
 
   const users = await fetchUsers();
 
-  //return Response.json({ users: users });
-  return NextResponse.redirect(new URL('/', req.url));
+  return Response.json({ users: users });
+  //return NextResponse.redirect(new URL('/', req.url));
+};
+
+export const POST = async (req: Request) => {
+  const body = await req.json();
+  const newUser = { ...body, id: Date.now().toString() };
+  await saveUser(newUser);
+  console.log(body);
+  return Response.json({ message: 'User created' });
 };
